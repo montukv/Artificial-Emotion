ae0efe0e417ca844d091863a7e077ff4e6d48c15
montukv <46618399+montukv@users.noreply.github.com>
2019-09-15 10:25:17 +0530

    Update twee.py.py

--- twee.py.py
+++ twee.py.py
@@ -1,7 +1,7 @@
 import tweepy
 from textblob import TextBlob
 
-# Step 1 - Authenticate of user
+#  Authenticate of user
 consumer_key= 'Your consumer key '
 consumer_secret= 'Your secret key'
 
@@ -13,7 +13,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-#Step 3 - Retrieve Tweets
+#Retrieve Tweets
 public_tweets = api.search('ajinkya')
 
 
@@ -21,7 +21,7 @@ public_tweets = api.search('ajinkya')
 for tweet in public_tweets:
     print(tweet.text)
     
-    #Step 4 Perform Sentiment Analysis on Tweets
+    #Perform Sentiment Analysis on Tweets
     analysis = TextBlob(tweet.text)
     print(analysis.sentiment)
     print("- ")
