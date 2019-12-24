1f7d3eba3146d8cefa678d94a5c2657cc8212755
montukv <46618399+montukv@users.noreply.github.com>
2019-12-24 15:43:35 +0530

    store latest id in other file

--- chatbot.py
+++ chatbot.py
@@ -21,7 +21,7 @@ def retrieve_last_seen_id(file_name):
 
 
 # store the last seen id for next time so that the same tweets are not accessed everytime
-def store_last_seen_id(last_seen_id, file_name):
+def store_last_seen_id(lastid, file_name):
     f_write = open(file_name, 'w')
     f_write.write(str(last_seen_id))
     f_write.close()
